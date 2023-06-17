ed44425153e15a5ecb8a14b9fdca3cbeaf2d488c
JoseAngel133 <zapotecas88@gmail.com>
2023-06-17 11:37:53 -0600

    Se modifico la vista productos

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Product from './Product'
-
+import Categorias from './Categoria'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
