3a359c315fa960c144ddf5819e2b4fc688247ccd
Adrian Moreno <luiadrimore219@gmail.com>
2023-06-16 23:20:15 -0600

    Implementación de la vista productos

---
+++ src/Product.jsx
@@ -0,0 +1,15 @@
+import Productos from './assets/components/Productos'
+import Footer from './assets/components/Footer'
+import Header from './assets/components/Header'
+import './assets/css/productos-style.css'
+
+const Product = () =>{
+    return(
+        <>
+         <Header />
+         <Productos />
+        </>
+    )
+}
+
+export default Product;
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Welcome from './Welcome.jsx'
 import Categoria from './Categoria.jsx'
+import Product from './Product.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Categoria />
-  </React.StrictMode>,
+    <Welcome />
+  </React.StrictMode>
 )
